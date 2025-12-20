b36901b85110db83ac31abfb7d3830eeda35a6dc
Hania <haniazipser2004@gmail.com>
2025-12-21 00:13:31 +0100

    refactor: move admin email to properties

--- Backend/src/main/kotlin/com/example/ePolan/Services/ExerciseApplicationService.java
+++ Backend/src/main/kotlin/com/example/ePolan/Services/ExerciseApplicationService.java
@@ -5,6 +5,7 @@ import com.example.ePolan.Model.Dtos.LessonDescriptionDto;
 import com.example.ePolan.Model.Entities.Exercise;
 import com.example.ePolan.Model.Entities.Lesson;
 import com.example.ePolan.Services.filegenerator.DocumentFormat;
+import com.example.ePolan.Services.filegenerator.DocumentService;
 import com.example.ePolan.Services.messagesender.AdminErrorMessage;
 import com.example.ePolan.Services.messagesender.EmailMessageSender;
 import com.example.ePolan.Services.messagesender.LessonListMessage;
@@ -12,6 +13,7 @@ import com.example.ePolan.Services.messagesender.Message;
 import com.itextpdf.text.DocumentException;
 import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -28,14 +30,17 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
 @Transactional
+@RequiredArgsConstructor
 public class ExerciseApplicationService {
+    @Value("${app.admin-email}")
+    private String adminEmail;
+
     private final EmailMessageSender emailSender;
     private final DocumentService documentService;
     private final ExerciseService exerciseService;
     private final LessonService lessonService;
-    @Value("${admin.email}") String adminEmail;
+
     @Retryable(retryFor = {MessagingException.class, FileNotFoundException.class, DocumentException.class})
     public void exportListToPdf(UUID lessonId){
         List<ExerciseWithPointsDto> exercises1 = exerciseService.getList(lessonId)
