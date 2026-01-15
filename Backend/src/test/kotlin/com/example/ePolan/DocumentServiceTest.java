669e5206c3a10d96bec895721dd177e86078a00c
Hania <haniazipser2004@gmail.com>
2026-01-15 18:19:34 +0100

    test: fix document service tests

--- Backend/src/test/kotlin/com/example/ePolan/DocumentServiceTest.java
+++ Backend/src/test/kotlin/com/example/ePolan/DocumentServiceTest.java
@@ -1,64 +1,65 @@
-//package com.example.ePolan;
-//
-//import com.example.ePolan.Model.Dtos.ExerciseWithPointsDto;
-//import com.example.ePolan.Model.Dtos.LessonDescriptionDto;
-//import com.example.ePolan.Services.filegenerator.DocumentFormat;
-//import com.example.ePolan.Services.filegenerator.DocumentGenerator;
-//import com.example.ePolan.Services.filegenerator.DocumentService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//
-//class DocumentServiceTest {
-//
-//    @Mock
-//    private DocumentGenerator pdfGenerator;
-//
-//    private DocumentService documentService;
-//
-//    private LessonDescriptionDto lesson;
-//    private List<ExerciseWithPointsDto> byExercise;
-//    private List<ExerciseWithPointsDto> byPoints;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//
-//        documentService = new DocumentService(Map.of(DocumentFormat.PDF, pdfGenerator));
-//
-//        lesson = new LessonDescriptionDto();
-//        byExercise = List.of();
-//        byPoints = List.of();
-//    }
-//
-//    @Test
-//    void testCreateDocument_delegatesToPdfGenerator() throws Exception {
-//
-//        when(pdfGenerator.generateDocument(byExercise, byPoints, lesson)).thenReturn("document.pdf");
-//
-//        String result = documentService.createDocument(DocumentFormat.PDF, byExercise, byPoints, lesson);
-//
-//
-//        assertEquals("document.pdf", result);
-//
-//
-//        verify(pdfGenerator).generateDocument(byExercise, byPoints, lesson);
-//    }
-//
-//    @Test
-//    void testCreateDocument_unsupportedFormat_throwsException() {
-//
-//        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
-//                documentService.createDocument(DocumentFormat.CSV, byExercise, byPoints, lesson)
-//        );
-//
-//        assertTrue(ex.getMessage().contains("Unsupported format"));
-//    }
-//}
+package com.example.ePolan;
+
+import com.example.ePolan.Model.Dtos.ExerciseWithPointsDto;
+import com.example.ePolan.Model.Dtos.LessonDescriptionDto;
+import com.example.ePolan.Services.filegenerator.DocumentFormat;
+import com.example.ePolan.Services.filegenerator.DocumentGenerator;
+import com.example.ePolan.Services.filegenerator.DocumentService;
+import com.example.ePolan.Services.filegenerator.PdfDocumentGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class DocumentServiceTest {
+
+    @Mock
+    private PdfDocumentGenerator pdfGenerator;
+
+    private DocumentService documentService;
+
+    private LessonDescriptionDto lesson;
+    private List<ExerciseWithPointsDto> byExercise;
+    private List<ExerciseWithPointsDto> byPoints;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        documentService = new DocumentService(pdfGenerator);
+
+        lesson = new LessonDescriptionDto();
+        byExercise = List.of();
+        byPoints = List.of();
+    }
+
+    @Test
+    void testCreateDocument_delegatesToPdfGenerator() throws Exception {
+
+        when(pdfGenerator.generateDocument(byExercise, byPoints, lesson)).thenReturn("document.pdf");
+
+        String result = documentService.createDocument(DocumentFormat.PDF, byExercise, byPoints, lesson);
+
+
+        assertEquals("document.pdf", result);
+
+
+        verify(pdfGenerator).generateDocument(byExercise, byPoints, lesson);
+    }
+
+    @Test
+    void testCreateDocument_unsupportedFormat_throwsException() {
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                documentService.createDocument(DocumentFormat.CSV, byExercise, byPoints, lesson)
+        );
+
+        assertTrue(ex.getMessage().contains("Unsupported format"));
+    }
+}
